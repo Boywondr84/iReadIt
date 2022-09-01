12020e69d95d9afa7040d7bba2e81cd4fb0f4bfb
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-08-31 19:12:10 -0500

    server & controller files

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
+// const session = require('express-session');
 // const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3005;
 
 const sequelize = require('./config/connection');
-const sequelizeStore = require('connect-session-sequelize')(session.Store);
+// const sequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const sess = {
-    secret: 'Secret super secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new sequelizeStore({
-        db: sequelize
-    })
-};
+// const sess = {
+//     secret: 'Secret super secret',
+//     cookie: {},
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new sequelizeStore({
+//         db: sequelize
+//     })
+// };
 
-app.use(session(sess));
+// app.use(session(sess));
 
 // app.engine('handlebars', hbs.engine);
 // app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // const routes = require('./controller');

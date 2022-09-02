e579971c89d53912103fcea9dbd79fcd2f4df7ab
Venus DM <crhowe90@gmail.com>
2022-09-02 15:17:50 -0500

    books route homepage, render books homepage

--- controller/home-routes.js
+++ controller/home-routes.js
@@ -1,6 +1,39 @@
 const router = require('express').Router();
 const { Book, Review, User } = require('../models');
 
+//get all books
+router.get('/', (req, res) => {
+    Book.findAll({
+        attributes:[
+            'id',
+            'title',
+            'user_id',
+            'created_at'
+        ],
+        include:[
+            {
+            model: Review,
+            attributes:['id', 'review_text', 'user_id', 'book_id'],
+            include:{
+                model:User,
+                attrubutes:['username']
+            }
+            },
+            {
+                model:User,
+                attributes:['username']
+            }
+        ]
+    })
+    .then(dbBookData => {
+        const books = dbBookData.map(book => book.get({plain: true}));
+        res.render('homepage', {books})
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 router.get('/', (req, res) => {
     Review.findAll({

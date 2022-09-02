d945ffd4217903eedbb8588269aa2430fe8b6da6
Venus DM <crhowe90@gmail.com>
2022-09-02 16:06:56 -0500

    single book

--- controller/home-routes.js
+++ controller/home-routes.js
@@ -4,65 +4,43 @@ const { Book, Review, User } = require('../models');
 //get all books
 router.get('/', (req, res) => {
     Book.findAll({
-        attributes:[
+        attributes: [
             'id',
             'title',
             'user_id',
             'created_at'
         ],
-        include:[
-            {
-            model: Review,
-            attributes:['id', 'review_text', 'user_id', 'book_id'],
-            include:{
-                model:User,
-                attrubutes:['username']
-            }
-            },
-            {
-                model:User,
-                attributes:['username']
-            }
-        ]
-    })
-    .then(dbBookData => {
-        const books = dbBookData.map(book => book.get({plain: true}));
-        res.render('homepage', {books})
-    })
-    .catch(err =>{
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
-
-router.get('/', (req, res) => {
-    Review.findAll({
-        attributes: [
-            "id", "review_text", "user_id", "book_id"
-        ],
         include: [
             {
-                model: Book,
-                attributes: ['id', 'title'],
+                model: Review,
+                attributes: ['id', 'review_text', 'user_id', 'book_id'],
                 include: {
                     model: User,
-                    attributes: ['username']
+                    attrubutes: ['username']
                 }
+            },
+            {
+                model: User,
+                attributes: ['username']
             }
         ]
     })
-    .then(dbData => {
-        const reviews = dbData.map(review => review.get({ plain: true }));
-
-        res.render('homepage', {
-            reviews,
-            loggedIn: req.session.loggedIn
+        .then(dbBookData => {
+            const books = dbBookData.map(book => book.get({ plain: true }));
+            res.render('homepage', { books })
         })
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
+
+
+
+
+
+
+
+
 
 module.exports = router;

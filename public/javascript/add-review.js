8b3bf987c9f90d70cc2a1a0553a6641645a0b1d7
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-09-03 11:04:27 -0500

    public js added files and edited code

--- public/javascript/add-review.js
+++ public/javascript/add-review.js
@@ -9,7 +9,7 @@
 //     ];
   
 //     if (comment_text) {
-//       const response = await fetch('/api/comments', {
+//       const response = await fetch('/api/review', {
 //         method: 'POST',
 //         body: JSON.stringify({
 //           post_id,
@@ -29,3 +29,18 @@
 //   }
   
 //   document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+
+
+// review handlebars 
+// <div class="reviews">
+//     {{#each this}}
+//     <section class="review">
+//     <div class="meta">
+//       {{user.username}} on {{created_at}}
+//     </div>
+//     <div class="text">
+//       {{review_text}}
+//     </div>
+//   </section>
+//   {{/each}}
+// </div>

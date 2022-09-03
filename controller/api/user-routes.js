de9be1bd10dac33db8fd5a48e022f41ee3d80536
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2022-09-03 14:31:28 -0500

    secure login and logout functioning with session

--- controller/api/user-routes.js
+++ controller/api/user-routes.js
@@ -1,57 +1,80 @@
-const router = require('express').Router();
-const { User } = require('../../models');
+const router = require("express").Router();
+const { User } = require("../../models");
 
-router.get('/', (req, res) => {
-    User.findAll({
-        attributes: {}
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: {},
+  })
+    .then((dbUserData) => {
+      res.json(dbUserData);
     })
-        .then(dbUserData => {
-            res.json(dbUserData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.post('/', (req, res) => {
-    User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
+//post route to create new user .then sign them in to a session...
+router.post("/", (req, res) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then((dbUserData) => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json({ user: dbUserData, message: "You are now logged in!" });
+        console.log("YOU are logged in!");
+      });
     })
-        .then(dbUserData => {
-            res.json(dbUserData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.post('/login', (req, res) => {
-    User.findOne({
-        where: {
-            email: req.body.email
-        }
-    })
-        .then(dbUserData => {
-            if (!dbUserData) {
-                res.status(400).json({ message: "User not found" });
-                return;
+router.post("/login", (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "User not found" });
+      return;
+    }
+    //validate user logic..
+    const validPassword = dbUserData.checkPassword(req.body.password);
 
-                // add validate password logic here later
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
 
-            } else {
-                res.json(dbUserData);
-            }
-        });
+      res.json({ user: dbUserData, message: "You are now logged in!" });
+      console.log(req.session);
+    });
+  });
 });
 
-router.post('/logout', (req, res) => {
-    // session destroy logic
-    res.status(204).end();
-
+//logout button on front end fetches with POST method to logout(destroy session)
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+      console.log("YOU are logged out!");
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;

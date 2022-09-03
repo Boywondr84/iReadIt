2227b971f6e06d79d3880bb525adb7841c0058aa
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-09-03 16:05:39 -0500

    commented out schema to make tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,18 @@
 DROP DATABASE IF EXISTS iReadit_db;
 
 CREATE DATABASE iReadit_db;
+
+-- CREATE TABLE books(
+--   id INTEGER NOT NULL,
+--   book_name VARCHAR(30) NOT NULL
+-- );
+
+-- CREATE TABLE reviews(
+--   id STRING NOT NULL,
+--   category_name VARCHAR(30) NOT NULL
+-- );
+
+-- CREATE TABLE users(
+--   ids STRING NOT NULL,
+--   price DECIMAL(10,2) NOT NULL
+-- );

9b60096a227b3ff253ba2b353d7628d63630eae1
Venus DM <crhowe90@gmail.com>
2022-09-03 18:03:37 -0500

    dashboard route user id books

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -17,7 +17,7 @@ async function signupFormHandler(event) {
     });
 
     if (response.ok) {
-      document.location.replace("/");
+      document.location.replace("/dashboard");
     } else {
       alert(response.statusText);
     }
@@ -41,7 +41,7 @@ async function loginFormHandler(event) {
     });
     console.log(response, response.ok);
     if (response.ok) {
-      document.location.replace("/");
+      document.location.replace("/dashboard");
     } else {
       alert(response.statusText);
     }

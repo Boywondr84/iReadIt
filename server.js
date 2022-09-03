6564aedc713c4eeb767c52ebe7e21f1abf078658
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-09-03 00:23:21 -0500

    server working commented out line 6 in bcrypt.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const path = require("path");
-// const express = npm irequire("express"); 
+const express = require("express"); 
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
